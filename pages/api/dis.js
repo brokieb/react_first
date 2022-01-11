56321169f71af39b58710ef70884d7a4efc7d758
brokieb <brokieb@gmail.com>
2022-01-11 14:54:40 +0100

    dis api

--- pages/api/dis.js
+++ pages/api/dis.js
@@ -1,6 +1,3 @@
 export default async (req, res) => {
-  if (req.method === "GET") {
-  } else {
-    return res.status(405).json({ err: "WRONG METHOD" });
-  }
+  return res.status(200).json("ALL DZIAŁA");
 };
